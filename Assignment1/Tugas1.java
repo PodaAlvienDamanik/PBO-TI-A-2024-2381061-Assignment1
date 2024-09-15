281c97fcb8909495f3c82a941dbd1586ec0bb75e
Alvin Damanik <2381061@unai.edu>
2024-09-15 14:54:44 +0700

    Tugas

--- Assignment1/Tugas1.java
+++ Assignment1/Tugas1.java
@@ -1,13 +1,29 @@
 package Assignment1;
 
+import java.util.Scanner;
+
 public class Tugas1 {
     public static void main(String[] args) {
-        int[] array={1,2,3};
+        Scanner scanner = new Scanner(System.in);
 
-        int jumlah = 0;
-        for (int bilangan : array){
-            jumlah += bilangan;
+        System.out.print("Masukkan jumlah elemen array: ");//meminta user memasukkan banyak array yang ingin dibuat
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];//mengubah angka input user ke dalam array
+
+        System.out.println("Masukkan " + n + " elemen array:");//Memasukkan angka isi array tersebut
+        for (int i = 0; i < n; i++) {
+            System.out.print("Elemen ke-" + (i + 1) + ": ");
+            array[i] = scanner.nextInt();//isi angka array nya
         }
-        System.out.println(jumlah);
+
+        int jumlah = 0;//dibuat 0 agar sama dengan jumlah array nya,sehingga ketika ditambah maka angka didalam array akan ditambah dengan indeks array tersebut
+        for (int i = 0; i < n; i++) {
+            jumlah += array[i];
+        }
+
+        System.out.println("Jumlah elemen array adalah: " + jumlah);
+
+        scanner.close();
     }
 }
